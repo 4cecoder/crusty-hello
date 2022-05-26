f58b47530870103f0d905389f48ee7c0808497b2
4cecoder <88108711+4cecoder@users.noreply.github.com>
2022-05-26 18:25:30 -0400

    fix: test for logic

--- src/testies.rs
+++ src/testies.rs
@@ -1,6 +1,13 @@
+// #[test]
+// pub fn test_yo_gangsta() -> std::io::Result<()> {
+//     let state = setup_dat_thang()?; // expected to succeed
+//     do_dat_thang(&state)?;          // expected to succeed
+//     Ok(())
+// }
+
 #[test]
-pub fn test_yo_gangsta() -> std::io::Result<()> {
-    let state = setup_dat_thang()?; // expected to succeed
-    do_dat_thang(&state)?;          // expected to succeed
+fn test_busting_off() -> std::io::Result<()> {
+    //let state = prepare_busting_off()?; // expected to succeed
+    just_bust()?;          // expected to succeed
     Ok(())
 }
